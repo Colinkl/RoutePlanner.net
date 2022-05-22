b200d05225e3c8dadf7665704b6755dd92826379
Cat <56826818+Colinkl@users.noreply.github.com>
2022-05-22 17:28:48 +0700

    test redesign

--- RoutePlanner.Tester/Program.cs
+++ RoutePlanner.Tester/Program.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using RoutePlanner.net;
 
-namespace RoutePlanner.Tester
+namespace RoutePlanner.App
 {
     class Program
     {
@@ -20,16 +20,17 @@ namespace RoutePlanner.Tester
                                                       new Connection(){ From ="7", ToRoute = "hub", Cost = 3, Custom = "d", Bidirectional = true},
                                                       new Connection(){ From ="hub", ToRoute = "2", Cost = 9, Custom = "d", Bidirectional = true},
                                                       new Connection(){ From ="hub", ToRoute = "5", Cost = 7, Custom = "d", Bidirectional = true},
-                                                      new Connection(){ From ="5", ToRoute = "8", Cost = 30, Custom = "d", Bidirectional = true},
-                                                      new Connection(){ From ="5", ToRoute = "3", Cost = 9, Custom = "d", Bidirectional = true},
-                                                      new Connection(){ From ="2", ToRoute = "3", Cost = 7, Custom = "d", Bidirectional = true},
+                                                      new Connection(){ From ="5", ToRoute = "8", Cost = 30, Custom = "d", Bidirectional = false},
+                                                      new Connection(){ From ="5", ToRoute = "3", Cost = 9, Custom = "d", Bidirectional = false},
+                                                      new Connection(){ From ="2", ToRoute = "3", Cost = 7, Custom = "d", Bidirectional = false},
                                                       new Connection(){ From ="4", ToRoute = "6", Cost = 3, Custom = "f", Bidirectional = true}};
-            var destList = new List<string> { "1", "8", "5", "3" };
+            var destList = new List<string> {"1", "2", "3", "4", "5", "6", "7", "8" };
             var a = route.GetRoute("hub", destList);
             foreach (var point in a)
             {
                 Console.WriteLine(point);
             }
+            //Console.ReadLine();
         }
     }
 }
